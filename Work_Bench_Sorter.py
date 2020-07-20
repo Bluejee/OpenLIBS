f7c8724d5ed90e195eaa1d4aa4443b9d88fe876d
Bluejee <balakrishnaprabhu1999@gmail.com>
2020-07-20 12:31:14 +0530

    Added a gitignore and created a workbench sorter for creating thr dateabase

---
+++ Work_Bench_Sorter.py
@@ -0,0 +1,22 @@
+"""
+
+This program aims to take in a CSV file named workbench in the Element_Database directory containing the persistent
+lines of an element and sort them and create a new CSV file named after the element.
+
+The program flow can be described as :
+1. Ask the user to give in the name of the element.
+2. Run the workbench through a sorter.
+3. create a new CSV file.
+
+"""
+
+import numpy as np
+
+print('Hey there!, Welcome.')
+element = input('Enter the name of the Element :: ')
+
+data = np.genfromtxt('Element_Database/Workbench.csv', delimiter=',')
+
+data.view('f8,f8,f8').sort(order=['f0'], axis=0)
+
+np.savetxt(element + '.csv', data, delimiter=',')
